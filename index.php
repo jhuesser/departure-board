601c7b924da259277be40aab1fefa58838a8b793
jhuesser <github@jhuesser.ch>
2019-03-10 21:13:21 +0100

    test output

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 $config = require_once "config.php";
-$station_board = json_decode(file_get_contents($config->api_root . "stationboard?station=" . urlencode($config->station)), true);
+if(isset($_GET['station'])){
+	$station = urlencode($_GET['station']);
+} else {
+	$station = urlencode($config->station);
+}
+$station_board = json_decode(file_get_contents($config->api_root . "stationboard?station=" . $station), true);
+
+foreach($station_board['stationboard'] as $departure){
+	echo date_format($departure['departureTimestamp'], 'H:i') . $departure['category'] . $departure['number'] . $departure['to'] . $departure['plattform'];
+}
+
 echo "<pre>"; print_r($station_board); echo "</pre>";
